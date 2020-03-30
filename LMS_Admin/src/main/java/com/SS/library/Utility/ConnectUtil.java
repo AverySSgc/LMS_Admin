9b676a70a6b07d256261b9c994f2240e840f4be2
AverySSgc <60415386+AverySSgc@users.noreply.github.com>
2020-03-30 13:59:37 -0400

    Add files via upload

---
+++ LMS_Admin/src/main/java/com/SS/library/Utility/ConnectUtil.java
@@ -0,0 +1,48 @@
+package com.SS.library.Utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class ConnectUtil {
+    private String driver= "com.mysql.cj.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost:3306/library?useSSL=false&allowPublicKeyRetrieval=true";
+    private String username = "root";
+    private String password = "password";
+//    private Connection conn =null;
+//    private static ConnectUtil instance;
+
+//    public ConnectUtil() throws SQLException, ClassNotFoundException {
+//        Class.forName(driver);
+//        Connection conn = DriverManager.getConnection(url,username,password);
+//        conn.setAutoCommit(false);
+//    }
+
+//    public static ConnectUtil getInstance() throws SQLException, ClassNotFoundException {
+//        if(instance==null){
+//            instance = new ConnectUtil();
+//        }
+//        return instance;
+//    }
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(driver);
+        Connection conn = DriverManager.getConnection(url,username,password);
+        conn.setAutoCommit(false);
+        return conn;
+    }
+
+//    public void deConsruct() throws SQLException {
+//        if(instance!=null) {
+//            instance.conn.close();
+//            instance.conn=null;
+//            instance = null;
+//        }
+//    }
+}
+
