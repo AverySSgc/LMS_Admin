5696fa43c6f6d5174a31c62400d5ba3dabd196db
AverySSgc <60415386+AverySSgc@users.noreply.github.com>
2020-03-30 13:32:59 -0400

    Update ConnectUtilTest.java
    temp test note out and delete unused test

--- LMS_Admin/src/test/java/com/SS/library/Utility/ConnectUtilTest.java
+++ LMS_Admin/src/test/java/com/SS/library/Utility/ConnectUtilTest.java
@@ -11,30 +11,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 class ConnectUtilTest {
 
-//	@Test
-//	void test() {	
-//		ConnectUtil connectUtil;
-//		try {
-//			connectUtil = new ConnectUtil();
-//			assertNotNull(connectUtil);	
-//		} catch (ClassNotFoundException | SQLException e) {
-//			// TODO Auto-generated catch block
-//			fail();
-//		}	
-//	}
-
-	@Test
-	void testGetConnection() {
-		try {
-			ConnectUtil connectUtil = new ConnectUtil();
-			Connection conn = connectUtil.getConnection();
-			assertNotNull(conn);
-			assertNotNull(connectUtil);	
-			conn.close();
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			fail();
-		}	
-	}
+// 	@Test
+// 	void testGetConnection() {
+// 		try {
+// 			ConnectUtil connectUtil = new ConnectUtil();
+// 			Connection conn = connectUtil.getConnection();
+// 			assertNotNull(conn);
+// 			assertNotNull(connectUtil);	
+// 			conn.close();
+// 		} catch (ClassNotFoundException | SQLException e) {
+// 			// TODO Auto-generated catch block
+// 			fail();
+// 		}	
+// 	}
 	
 }
